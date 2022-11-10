b71e4fe690251ec2cdf6cd965de05072110fdea0
Agbesi Amenyo <56748821+agbesi-amenyo@users.noreply.github.com>
2022-11-10 19:35:24 +0200

    adding and deleting blogs possible

--- src/Create.js
+++ src/Create.js
@@ -4,12 +4,29 @@ const Create = () => {
     const [title, setTitle ]= useState(''); 
     const [body, setBody ]= useState(''); 
     const [author, setAuthor ]= useState('mario'); 
+    const [isPending, setIsPending]= useState(false); 
+
+    const handleSubmit = (e)=>{
+        e.preventDefault(); 
+        const blog = {title, body, author}; 
+        setIsPending(true); 
+        
+        fetch("http://localhost:8000/blogs", {
+            method: 'POST', 
+            headers : {"Content-type": "application/json"},
+            body : JSON.stringify(blog)
+        }).then(()=>{
+            console.log("new blog added!");
+            setIsPending(false); 
+        })
+    }
+
     return ( 
 
         <div className="create">
             <h2>Add a New Blog</h2>
 
-            <form>
+            <form onSubmit = {handleSubmit} >
                 <label>Blog title : </label>
                 <input 
                 type="text" 
@@ -34,10 +51,9 @@ const Create = () => {
                 <option value="mario">Mario</option>
                 <option value="yoshi">Yoshi</option>
                 </select  >
-                <button>Add Blog </button>
-                <p>{title}</p>
-                <p>{body}</p>
-                <p>{author}</p>
+                {!isPending && <button>Add Blog </button>}
+                {isPending && <button>Adding blog...</button>}
+                
             </form>
         </div>
 
